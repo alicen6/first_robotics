37f7a5ea2bb700182694227a8108e50afa8184f9
alicen <alicen@cobalt5.com>
2016-02-21 20:12:50 -0800

    got team_stats working and able to see information in the database

--- frcstats/views.py
+++ frcstats/views.py
@@ -5,30 +5,19 @@ from django.forms import ModelForm, modelform_factory
 from .models import Team, Match
 from forms import MatchForm
 from django.views.generic import View
+from django import forms
 
 
 def get_name(request):
     TeamForm = modelform_factory(Team, fields='__all__')
     if request.method == 'POST':
-        team_form = TeamForm(request.POST, request.FILES)
+        team_form = Team(request.POST, request.FILES)
         if team_form.is_valid():
             team_form.save()
-            return render(
-                request,
-                'confirmation.html',
-                {
-                    'team_form': team_form,
-                }
-            )
+            return render(request, 'confirmation.html', {'team_form': team_form,})
     else:
         team_form = TeamForm()
-    return render(
-        request,
-        'team_info.html',
-        {
-            'team_form': team_form,
-        }
-    )
+    return render(request, 'team_info.html', {'team_form': team_form})
 
 
 def get_match(request):
@@ -37,25 +26,30 @@ def get_match(request):
         match_form = MatchForm(request.POST, request.FILES)
         if match_form.is_valid():
             match_form.save()
-            return render(
-                request,
-                'confirmation.html',
-                {
-                    'match_form': match_form
-                }
-            )
+            return render(request, 'confirmation.html', {'match_form': match_form})
     else:
         match_form = MatchForm()
-    return render(
-        request,
-        'match.html',
-        {
-            'match_form': match_form
-        }
-    )
+    return render(request, 'match.html', {'match_form': match_form})
+
+
+def team_stats(request):
+    class TeamNumberForm(forms.Form):
+        team_number = forms.IntegerField()
+    if request.method == 'GET':
+        form = TeamNumberForm()
+    else:
+        # A POST request: Handle Form Upload
+        form = TeamNumberForm(request.POST)
+        # If data is valid, proceeds to create a new post and redirect the user
+        if form.is_valid():
+            team_number = form.cleaned_data['team_number']
+            return HttpResponseRedirect('/team-stats/' + str(team_number))
+    return render(request, 'team_number.html', {'team_number_form': form})
+
 
 def team_stats_from_team_number(request, team_number):
-    team = get_object_or_404(Team, team_number=team_number)
-    print(team_number)
+    team = Team.objects.filter(team_number=team_number)
+    matches = Match.objects.filter(team_number=team[0].id)
+    #team = get_object_or_404(Team, team_number=team_number)
     # team_number is the variable that will now allow you to do all the things
-    return render(request, 'team-stats.html', {'teams': team.team_number})
+    return render(request, 'team-stats.html', {'teams': team, 'matches': matches})
