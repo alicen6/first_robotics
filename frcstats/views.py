8f466554db992ffc0bb926c98519c5653a957a7b
alicen <alicen@cobalt5.com>
2016-02-23 14:16:13 -0800

    clean up views.py, add failure page if no team info yet

--- frcstats/views.py
+++ frcstats/views.py
@@ -57,7 +57,7 @@ def team_stats_from_team_number(request, team_number):
 
     def stuck_divide(values, stuck):
         return str(int(((float(len(stuck)) /
-                                      (float(len(values))) if len(values) != 0 else 0) + float(len(stuck))) * 100)) + "%"
+                         (float(len(values))) if len(values) != 0 else 0) + float(len(stuck))) * 100)) + "%"
     team = Team.objects.filter(team_number=team_number)
     matches = Match.objects.filter(team_number=team[0].id)
     auton_low_values = []
@@ -86,141 +86,134 @@ def team_stats_from_team_number(request, team_number):
     lowbar_stuck_stats = []
     ramparts_stats = []
     ramparts_stuck_stats = []
-    for match in matches:
-        if match.teleop_portc == 1:
-            portc_stuck_stats.append(match.teleop_portc)
-        elif match.teleop_portc != 0:
-            portc_stats.append(match.teleop_portc)
-        else:
-            pass
-        if match.teleop_drawb == 1:
-            drawb_stuck_stats.append(match.teleop_drawb)
-        elif match.teleop_drawb != 0:
-            drawb_stats.append(match.teleop_drawb)
-        else:
-            pass
-        if match.teleop_cdf == 1:
-            cdf_stuck_stats.append(match.teleop_cdf)
-        elif match.teleop_cdf != 0:
-            cdf_stats.append(match.teleop_cdf)
-        else:
-            pass
-        if match.teleop_moat == 1:
-            moat_stuck_stats.append(match.teleop_moat)
-        elif match.teleop_moat != 0:
-            moat_stats.append(match.teleop_moat)
-        else:
-            pass
-        if match.teleop_sallyp == 1:
-            sallyp_stuck_stats.append(match.teleop_sallyp)
-        elif match.teleop_sallyp != 0:
-            sallyp_stats.append(match.teleop_sallyp)
-        else:
-            pass
-        if match.teleop_rought == 1:
-            rought_stuck_stats.append(match.teleop_rought)
-        elif match.teleop_rought != 0:
-            rought_stats.append(match.teleop_rought)
-        else:
-            pass
-        if match.teleop_lowbar == 1:
-            lowbar_stuck_stats.append(match.teleop_lowbar)
-        elif match.teleop_lowbar != 0:
-            lowbar_stats.append(match.teleop_lowbar)
-        else:
-            pass
-        if match.teleop_ramparts == 1:
-            ramparts_stuck_stats.append(match.teleop_ramparts)
-        elif match.teleop_ramparts != 0:
-            ramparts_stats.append(match.teleop_ramparts)
-        else:
-            pass
-        if match.hang_input == 1:
-            hang_input_values.append(match.hang_input)
-        elif match.hang_input == 2:
-            hang_success_values.append(match.hang_input)
-        else:
-            hang_fail_values.append(match.hang_input)
+    try:
+        for match in matches:
+            if match.teleop_portc == 1:
+                portc_stuck_stats.append(match.teleop_portc)
+            elif match.teleop_portc != 0:
+                portc_stats.append(match.teleop_portc)
+            else:
+                pass
+            if match.teleop_drawb == 1:
+                drawb_stuck_stats.append(match.teleop_drawb)
+            elif match.teleop_drawb != 0:
+                drawb_stats.append(match.teleop_drawb)
+            else:
+                pass
+            if match.teleop_cdf == 1:
+                cdf_stuck_stats.append(match.teleop_cdf)
+            elif match.teleop_cdf != 0:
+                cdf_stats.append(match.teleop_cdf)
+            else:
+                pass
+            if match.teleop_moat == 1:
+                moat_stuck_stats.append(match.teleop_moat)
+            elif match.teleop_moat != 0:
+                moat_stats.append(match.teleop_moat)
+            else:
+                pass
+            if match.teleop_sallyp == 1:
+                sallyp_stuck_stats.append(match.teleop_sallyp)
+            elif match.teleop_sallyp != 0:
+                sallyp_stats.append(match.teleop_sallyp)
+            else:
+                pass
+            if match.teleop_rought == 1:
+                rought_stuck_stats.append(match.teleop_rought)
+            elif match.teleop_rought != 0:
+                rought_stats.append(match.teleop_rought)
+            else:
+                pass
+            if match.teleop_lowbar == 1:
+                lowbar_stuck_stats.append(match.teleop_lowbar)
+            elif match.teleop_lowbar != 0:
+                lowbar_stats.append(match.teleop_lowbar)
+            else:
+                pass
+            if match.teleop_ramparts == 1:
+                ramparts_stuck_stats.append(match.teleop_ramparts)
+            elif match.teleop_ramparts != 0:
+                ramparts_stats.append(match.teleop_ramparts)
+            else:
+                pass
+            if match.hang_input == 1:
+                hang_input_values.append(match.hang_input)
+            elif match.hang_input == 2:
+                hang_success_values.append(match.hang_input)
+            else:
+                hang_fail_values.append(match.hang_input)
 
-        auton_low_values.append(match.auton_low_goals)
-        auton_high_values.append(match.auton_high_goals)
-        teleop_low_values.append(match.teleop_low_goals)
-        teleop_high_values.append(match.teleop_high_goals)
-        auton_def_reached_values.append(match.auton_def_reached)
-        auton_def_crossed_values.append(match.auton_def_crossed)
-        played_def_values.append(match.played_def)
-        auton_low_stats = sum(auton_low_values) / float(len(auton_low_values))
-        auton_high_stats = sum(auton_high_values) / \
-            float(len(auton_high_values))
-    teleop_low_stats = sum(teleop_low_values) / float(len(teleop_low_values))
-    teleop_high_stats = sum(teleop_high_values) / \
-        float(len(teleop_high_values))
-    portc_stats_value = divide(portc_stats)
-    portc_stuck_stats = stuck_divide(portc_stats, portc_stuck_stats)
-    drawb_stats_value = divide(drawb_stats)
-    drawb_stuck_stats = stuck_divide(drawb_stats, drawb_stuck_stats)
-    cdf_stats_value = divide(cdf_stats)
-    cdf_stuck_stats = stuck_divide(cdf_stats, cdf_stuck_stats)
-    moat_stats_value = divide(moat_stats)
-    moat_stuck_stats = stuck_divide(moat_stats, moat_stuck_stats)
-    sallyp_stats_value = divide(sallyp_stats)
-    sallyp_stuck_stats = stuck_divide(sallyp_stats, sallyp_stuck_stats)
-    rought_stats_value = divide(rought_stats)
-    rought_stuck_stats = stuck_divide(rought_stats, rought_stuck_stats)
-    lowbar_stats_value = divide(lowbar_stats)
-    lowbar_stuck_stats = stuck_divide(lowbar_stats, lowbar_stuck_stats)
-    ramparts_stats_value = divide(ramparts_stats)
-    ramparts_stuck_stats = stuck_divide(ramparts_stats, ramparts_stuck_stats)
-    hang_value = str(
-        int(((float(len(hang_input_values)) + float(len(hang_success_values))) /
-             (float(len(hang_input_values)) + float(len(hang_success_values)) + float(len(hang_fail_values)))) * 100)) + "%"
-    hang_success_stats = str(int(float(len(hang_success_values)) / (
-        float(len(hang_input_values)) + float(len(hang_success_values))) * 100)) + "%"
-    hang_fail = str(int((float(len(hang_fail_values)) /
-                         float(len(hang_input_values) + len(hang_fail_values))) * 100)) + "%"
-    played_def_stats = str(
-        int((sum(played_def_values) / float(len(played_def_values))) * 100)) + "%"
+            auton_low_values.append(match.auton_low_goals)
+            auton_high_values.append(match.auton_high_goals)
+            teleop_low_values.append(match.teleop_low_goals)
+            teleop_high_values.append(match.teleop_high_goals)
+            auton_def_reached_values.append(match.auton_def_reached)
+            auton_def_crossed_values.append(match.auton_def_crossed)
+            played_def_values.append(match.played_def)
+            auton_low_stats = sum(auton_low_values) / float(len(auton_low_values))
+            auton_high_stats = sum(auton_high_values) / \
+                float(len(auton_high_values))
+        teleop_low_stats = sum(teleop_low_values) / float(len(teleop_low_values))
+        teleop_high_stats = sum(teleop_high_values) / \
+            float(len(teleop_high_values))
+        portc_stats_value = divide(portc_stats)
+        portc_stuck_stats = stuck_divide(portc_stats, portc_stuck_stats)
+        drawb_stats_value = divide(drawb_stats)
+        drawb_stuck_stats = stuck_divide(drawb_stats, drawb_stuck_stats)
+        cdf_stats_value = divide(cdf_stats)
+        cdf_stuck_stats = stuck_divide(cdf_stats, cdf_stuck_stats)
+        moat_stats_value = divide(moat_stats)
+        moat_stuck_stats = stuck_divide(moat_stats, moat_stuck_stats)
+        sallyp_stats_value = divide(sallyp_stats)
+        sallyp_stuck_stats = stuck_divide(sallyp_stats, sallyp_stuck_stats)
+        rought_stats_value = divide(rought_stats)
+        rought_stuck_stats = stuck_divide(rought_stats, rought_stuck_stats)
+        lowbar_stats_value = divide(lowbar_stats)
+        lowbar_stuck_stats = stuck_divide(lowbar_stats, lowbar_stuck_stats)
+        ramparts_stats_value = divide(ramparts_stats)
+        ramparts_stuck_stats = stuck_divide(ramparts_stats, ramparts_stuck_stats)
+        hang_value = str(
+            int(((float(len(hang_input_values)) + float(len(hang_success_values))) /
+                 (float(len(hang_input_values)) + float(len(hang_success_values)) + float(len(hang_fail_values)))) * 100)) + "%"
+        hang_success_stats = str(int(float(len(hang_success_values)) / (
+            float(len(hang_input_values)) + float(len(hang_success_values))) * 100)) + "%"
+        hang_fail = str(int((float(len(hang_fail_values)) /
+                             float(len(hang_input_values) + len(hang_fail_values))) * 100)) + "%"
+        played_def_stats = str(
+            int((sum(played_def_values) / float(len(played_def_values))) * 100)) + "%"
 
-    # team = get_object_or_404(Team, team_number=team_number)
-    # team_number is the variable that will now allow you to do all the things
-    """
-    l = [["hi", "there"], ["hello", "their"]]
-    [x[0] for x in l]
-    for x in l:
-        print x[0]
-
-    d = {"hi": "there", "hello": "their"}
-    {x for x in d}
-    for x, y in d.iteritems():
-        print x, y
-    """
-    return render(request, 'team-stats.html', {'teams': team,
-                                               'hang_value': hang_value, 'hang_fail': hang_fail,
-                                               'auton_low_stats': auton_low_stats,
-                                               'auton_high_stats': auton_high_stats,
-                                               'teleop_low_stats': teleop_low_stats,
-                                               'teleop_high_stats': teleop_high_stats,
-                                               'auton_def_reached_values': list({auton_def_choices[x] for x in auton_def_reached_values}),
-                                               'auton_def_crossed_values': list({auton_def_choices[x] for x in auton_def_crossed_values}),
-                                               'played_def_stats': played_def_stats,
-                                               'portc_stats_value': portc_stats_value,
-                                               'portc_stuck_stats': portc_stuck_stats,
-                                               'drawb_stats_value': drawb_stats_value,
-                                               'drawb_stuck_stats': drawb_stuck_stats,
-                                               'cdf_stats_value': cdf_stats_value,
-                                               'cdf_stuck_stats': cdf_stuck_stats,
-                                               'moat_stats_value': moat_stats_value,
-                                               'moat_stuck_stats': moat_stuck_stats,
-                                               'sallyp_stats_value': sallyp_stats_value,
-                                               'sallyp_stuck_stats': sallyp_stuck_stats,
-                                               'rought_stats_value': rought_stats_value,
-                                               'rought_stuck_stats': rought_stuck_stats,
-                                               'lowbar_stats_value': lowbar_stats_value,
-                                               'lowbar_stuck_stats': lowbar_stuck_stats,
-                                               'ramparts_stats_value': ramparts_stats_value,
-                                               'ramparts_stuck_stats': ramparts_stuck_stats,
-                                               'hang_success_stats': hang_success_stats
-                                               })
+        # team = get_object_or_404(Team, team_number=team_number)
+        # team_number is the variable that will now allow you to do all the things
+        return render(request, 'team-stats.html', {'teams': team,
+                                                   'hang_value': hang_value, 'hang_fail': hang_fail,
+                                                   'auton_low_stats': auton_low_stats,
+                                                   'auton_high_stats': auton_high_stats,
+                                                   'teleop_low_stats': teleop_low_stats,
+                                                   'teleop_high_stats': teleop_high_stats,
+                                                   'auton_def_reached_values': list({auton_def_choices[x] for x in auton_def_reached_values}),
+                                                   'auton_def_crossed_values': list({auton_def_choices[x] for x in auton_def_crossed_values}),
+                                                   'played_def_stats': played_def_stats,
+                                                   'portc_stats_value': portc_stats_value,
+                                                   'portc_stuck_stats': portc_stuck_stats,
+                                                   'drawb_stats_value': drawb_stats_value,
+                                                   'drawb_stuck_stats': drawb_stuck_stats,
+                                                   'cdf_stats_value': cdf_stats_value,
+                                                   'cdf_stuck_stats': cdf_stuck_stats,
+                                                   'moat_stats_value': moat_stats_value,
+                                                   'moat_stuck_stats': moat_stuck_stats,
+                                                   'sallyp_stats_value': sallyp_stats_value,
+                                                   'sallyp_stuck_stats': sallyp_stuck_stats,
+                                                   'rought_stats_value': rought_stats_value,
+                                                   'rought_stuck_stats': rought_stuck_stats,
+                                                   'lowbar_stats_value': lowbar_stats_value,
+                                                   'lowbar_stuck_stats': lowbar_stuck_stats,
+                                                   'ramparts_stats_value': ramparts_stats_value,
+                                                   'ramparts_stuck_stats': ramparts_stuck_stats,
+                                                   'hang_success_stats': hang_success_stats
+                                                   })
+    except:
+        context = {}
+        return render(request, 'no-team.html', context)
 
 
 def get_home(request):
